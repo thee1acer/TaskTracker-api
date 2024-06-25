3a3a2ae6139468b9a5ca95bdeb1edfa570146f6c
Mpho Nkala <mphon@synthesis.co.za>
2024-06-25 03:10:43 +0200

    added inactive field to task entity

---
+++ TaskTracker.Database/Migrations/20240625011000_AddedInActiveFieldToTaskEntity.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaskTracker.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedInActiveFieldToTaskEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Inactive",
+                table: "ApplicationUser",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Inactive",
+                table: "ApplicationUser");
+        }
+    }
+}
