cad694cfe9ee17b80f3bf75a9ecd50f85539ae6e
Mpho Nkala <mphon@synthesis.co.za>
2024-06-30 20:43:38 +0200

    fix mismatch between schema and dto

--- TaskTracker.Common/Models/Task/TaskEntityDTO.cs
+++ TaskTracker.Common/Models/Task/TaskEntityDTO.cs
@@ -6,8 +6,8 @@
         public int AssignedTo {  get; set; }
         public string ShortDescription { get; set; }
         public string DetailedDescription { get; set; }
-        public int State { get; set; }
-        public int TaskType { get; set; }
+        public string State { get; set; }
+        public string TaskType { get; set; }
         public int TaskPriority { get; set; }
         public int TaskStoryPoints { get; set; }
         public int TaskStoryEffort { get; set; }
